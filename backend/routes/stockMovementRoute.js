8b23cc678569cc6eb5e4aad4dd393cda32eb1a4a
xevy1399 <immartsevi17@gmail.com>
2025-02-17 09:49:19 +0800

    removed backend folder

--- backend/routes/stockMovementRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const stockMovementController = require("../controllers/stockMovementController");
-
-// Routes
-router.post("/", stockMovementController.addStockMovement);
-router.get("/", stockMovementController.getStockMovements);
-
-module.exports = router;
